55c3fdf4869d5b0fc1bebde49f602beaa35745ab
Yuki Chikujoin <User.yukisnow@gmail.com>
2021-09-30 15:21:45 +0200

    Created Project
    Start

---
+++ pollstar/src/test/java/com/Toine/pollstar/PollstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Toine.pollstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
