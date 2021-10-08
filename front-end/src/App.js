cc8b20fee588f0539a681c1f5d43c724621f5cad
Yuki Chikujoin <User.yukisnow@gmail.com>
2021-10-09 00:36:35 +0200

    Last Commit I forgot to do..

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/Test.js';
 
-function App() {
+import axios from "axios";
+import React from "react";
+import { Input } from 'reactstrap';
+//import Test from './components/Test.js';
+//import React, {Component} from 'react';
+
+const baseURL = "http://localhost:8080/";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+  const [pollID, setPollID] = React.useState();
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}poll/0`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function readPost()
+  {
+    axios.get(`${baseURL}poll/` + pollID).then((response) => {
+      setPost(response.data);
+    });
+  }
+
+  function createPost() {
+    axios
+      .post(`${baseURL}poll/`, {
+        pollName: "Test Poll: "+ pollID,
+        pollID: pollID
+      })
+      .then((response) => {
+        console.log(response);
+        axios.get(`${baseURL}`+response.data).then((res) => {
+          setPost(res.data);
+        });
+      });
+  }
+
+  if (!post) return "No post!"
+
+  return (
+    <div>
+      
+
+
+      <h1>Poll Name = {post.pollName}</h1>
+      <h1>Poll ID = {post.pollID}</h1>
+      <h1>Poll Created on = {post.pollCreationDate}</h1>
+      <Input placeholder="Poll ID" min={0} max={100} type="number" onChange={event => setPollID(event.target.value)}/>
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={readPost}>Read Post</button>
+    </div>
+  );
+}
+
+/*class App extends Component {
+  state = {
+    polls: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/poll')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ polls: data })
+    })
+    .catch(console.log)
+  }
+
+  render () {
+    return (
+      <Polls polls={this.state.polls} />
+    )
+  }
+}
+
+export default App;
+
+/*function App() {
   return (
     <div className="App">
       <Test/>
@@ -10,4 +88,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
