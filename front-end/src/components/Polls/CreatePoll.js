b08ef2518d94d5472f45ced9aba4c37e28f71cd1
Yuki Chikujoin <User.yukisnow@gmail.com>
2022-01-10 12:31:25 +0100

    Updated site
    added fucntionality, dynamically add options

--- front-end/src/components/Polls/CreatePoll.js
+++ front-end/src/components/Polls/CreatePoll.js
@@ -1,84 +1,123 @@
 import axios from "axios";
-import React from "react";
-import { Input, Form, FormGroup, Label } from 'reactstrap';
+import React, { useState } from "react";
+import { Input, Form, FormGroup, Label, Button } from 'reactstrap';
 import { BrowserRouter as useHistory, Redirect, Router, Switch, Route, Link } from "react-router-dom";
 import ViewPoll from "./ViewPoll";
 //import React, {Component} from 'react';
 
-import { useCookies } from 'react-cookie';
+
 
 
 const baseURL = "http://localhost:8080/";
 
 const CreatePoll = (props) => {
-  const [cookies, setCookie] = useCookies(['name']);
   
+
   //const [post, setPost] = React.useState(null);
   const [pollID, setPollID] = React.useState();
   const [PostedID, setID] = React.useState();
+  const [inputList, setInputList] = useState([{ choiceName: "" }]);
+
   const JWT = localStorage.getItem("JWT");
+
+    // handle input change
+    const handleInputChange = (e, index) => {
+      const { name, value } = e.target;
+      const list = [...inputList];
+      list[index][name] = value;
+      setInputList(list);
+    };
+
+      // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+ 
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { choiceName: "" }]);
+  };
 
   function createPost() {
     //document.getElementById("pollName").value; krijg poll input box value
-var today = new Date();
-var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
-//document.getElementById("o1").value = date;
-let pollChoicess = 
-  [
-    { 
-      choiceName: document.getElementById("o1").value
-    }, 
-    {
-      choiceName: document.getElementById("o2").value
-    }
-  ]
+    var today = new Date();
+    var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
 
-  let headerConfig = {
-    headers: {
-      Authorization: JWT
+    let pollChoicess = 
+      [
+        { 
+          choiceName: document.getElementById("o1").value
+        }, 
+        {
+          choiceName: document.getElementById("o2").value
+        }
+      ]
+
+    let headerConfig = {
+      headers: {
+        Authorization: JWT
+      }
     }
-  }
-//document.getElementById("o1").value = JSON.stringify(pollChoices);
-console.log(pollChoicess);
-    axios
-      .post(`${baseURL}poll/`, {
-        pollName: document.getElementById("pollName").value,
-        //pollID: pollID,
-        pollCreationDate: date,
-        pollChoices: pollChoicess
-      }, headerConfig)
-      .then((response) => {
-        setID("/ViewPoll/" + response.data);
-        axios.get(`${baseURL}poll/`+response.data).then((res) => {
-          
+  //document.getElementById("o1").value = JSON.stringify(pollChoices);
+  console.log(pollChoicess.toString())
+  console.log(JSON.stringify(inputList))
+      axios
+        .post(`${baseURL}poll/`, {
+          pollName: document.getElementById("pollName").value,
+          //pollID: pollID,
+          pollCreationDate: date,
+          pollChoices: inputList
+        }, headerConfig)
+        .then((response) => {
+          setID("/ViewPoll/" + response.data);
+          axios.get(`${baseURL}poll/`+response.data).then((res) => {
+            
+          });
         });
-      });
-      
-      
   }
 
   if(PostedID != undefined) return(<Redirect to={PostedID} />) 
 
   return (
     <div>
-    <h4>{cookies.name}</h4>
-
-    <Form>
         <FormGroup>
             <Label for="pollId">Poll ID:</Label>
             <Input type="number" name="poll" id="pollId" placeholder="Enter id of Poll" onChange={event => setPollID(event.target.value)}/>
+            </FormGroup>
+            <FormGroup>
             <Label for="pollName">Poll name:</Label>
             <Input type="text" name="poll" id="pollName" placeholder="Enter name of Poll"/>
-
-            <Label for="pollOption">Option 1:</Label>
-            <Input type="text" name="option" id="o1" placeholder="Enter option 1"/>
-            <Label for="pollOption">Option2:</Label>
-            <Input type="text" name="option" id="o2" placeholder="Enter option 2"/>
-         </FormGroup>
+            </FormGroup>
+            
+            <Button onClick={handleAddClick}>Add</Button>
+            {inputList.map((x, i) => {
+              
+              return (
+                <div className="box">
+                  <FormGroup>
+                  <Label for={"choice"+i.toString()}>Option {i + 1}:</Label>
+                  <Input
+                    name="choiceName"
+                    
+                    placeholder="Enter Option Name"
+                    value={x.choiceName}
+                    onChange={e => handleInputChange(e, i)}
+                  />
+                  <div className="btn-box">
+                    {inputList.length !== 1 && <Button
+                      className="mr10"
+                      onClick={() => handleRemoveClick(i)}>Remove</Button>}
+                  </div>
+                  </FormGroup>
+                </div>
+              );
+            })}
         
-    </Form>
     
-    <button onClick={createPost}>Savu!</button>
+    
+    <Button onClick={createPost}>Savu!</Button>
     </div>
   );
 }
