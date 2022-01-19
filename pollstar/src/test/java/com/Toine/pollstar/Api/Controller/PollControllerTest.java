6a6bb14831f69f3ba24c6eac33267f9e5fafcf73
Yuki Chikujoin <User.yukisnow@gmail.com>
2022-01-19 17:12:25 +0100

    Added all classes
    jsut fill em in

---
+++ pollstar/src/test/java/com/Toine/pollstar/Api/Controller/PollControllerTest.java
@@ -0,0 +1,28 @@
+package com.Toine.pollstar.Api.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PollControllerTest {
+
+    @Test
+    void confirmTest() {
+    }
+
+    @Test
+    void getPoll() {
+    }
+
+    @Test
+    void createPoll() {
+    }
+
+    @Test
+    void addVoter() {
+    }
+
+    @Test
+    void getAllPolls() {
+    }
+}
